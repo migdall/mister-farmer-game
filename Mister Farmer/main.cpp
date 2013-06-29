ba59db8b4007870ef14999a987cf7a54df1331a6
Mehryn <mehryn@genesis.local>
2013-06-28 17:36:52 -0700

    Initial Commit

---
+++ Mister Farmer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Mister Farmer
+//
+//  Created by Mehryn on 6/28/13.
+//  Copyright (c) 2013 Reverent Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
