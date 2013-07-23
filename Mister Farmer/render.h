6977bed1da615a814c6ed1f4dd13bd6b3e482ab3
Mehryn <mehryn@genesis.local>
2013-07-23 12:47:11 -0700

    Adding the new license and beginnings of the rendering system.

---
+++ Mister Farmer/render.h
@@ -0,0 +1,41 @@
+/*
+ 
+ Copyright (c) 2013 Jesus A. Noland
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ 
+ render.h
+ Mister Farmer
+ 
+ Created by Jesus A. Noland on 7/23/13.
+ 
+ */
+
+#ifndef Mister_Farmer_render_h
+#define Mister_Farmer_render_h
+
+#include "SDL/SDL.h"
+#include "SDL_image/SDL_image.h"
+#include "SDL_ttf/SDL_ttf.h"
+
+#include <vector>
+
+std::vector<SDL_Surface*> renderBin;
+
+#endif
